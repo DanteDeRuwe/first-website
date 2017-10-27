23c773d3ab7a5866834096fef1553a9d364fe5e2
Dante De Ruwe <dantederuwe@gmail.com>
2017-10-28 01:19:20 +0200

    changed the zoom mechanic

--- p/solar-system/scripts/planets.js
+++ p/solar-system/scripts/planets.js
@@ -5,19 +5,15 @@ function Orbit(a,e) {
     this.a = a;
     this.e = e;
 
-    // map values between 0 and when for a max  a+ae  = width/2
-    //this.a = map(this.a, 0 , 30.1 , 0 , (1+this.e) * 30.1 * 215 * solar_r);
     //calculate b
     this.b = this.a * Math.sqrt(1- (this.e * this.e));
-
-
 
     this.show = function () {
         noFill();
         stroke('#fff');
         this.x = fromAUtoPx(this.a) * this.e + width/2;
         this.y = height/2;
-        ellipse(this.x,this.y , fromAUtoPx(this.a) , fromAUtoPx(this.b));
+        ellipse(this.x,this.y , 2* fromAUtoPx(this.a) , 2* fromAUtoPx(this.b));
     }
 }
 
@@ -25,34 +21,34 @@ function showOrbits(mer, ven, ter, mar, jup, sat, ura, nep) {
     //Orbits
 
 //Mercury
-    merO = new Orbit(data.mer.elements[0].a, data.mer.elements[0].e);
+    var merO = new Orbit(data.mer.elements[0].a, data.mer.elements[0].e);
     if(mer){merO.show();}
 
 //Venus
-    venO = new Orbit(data.ven.elements[0].a, data.ven.elements[0].e);
+    var venO = new Orbit(data.ven.elements[0].a, data.ven.elements[0].e);
     if(ven){venO.show();}
 
 //Earth
-    terO = new Orbit(data.ter.elements[0].a, data.ter.elements[0].e);
+    var terO = new Orbit(data.ter.elements[0].a, data.ter.elements[0].e);
     if(ter){terO.show();}
 
 //Mars
-    marO = new Orbit(data.mar.elements[0].a, data.mar.elements[0].e);
+    var marO = new Orbit(data.mar.elements[0].a, data.mar.elements[0].e);
     if(mar){marO.show();}
 
 //Jupiter
-    jupO = new Orbit(data.jup.elements[0].a, data.jup.elements[0].e);
+    var jupO = new Orbit(data.jup.elements[0].a, data.jup.elements[0].e);
     if(jup){jupO.show();}
 
 //Saturn
-    satO = new Orbit(data.sat.elements[0].a, data.sat.elements[0].e);
+    var satO = new Orbit(data.sat.elements[0].a, data.sat.elements[0].e);
     if(sat){satO.show();}
 
 //Uranus
-    uraO = new Orbit(data.ura.elements[0].a, data.ura.elements[0].e);
+    var uraO = new Orbit(data.ura.elements[0].a, data.ura.elements[0].e);
     if(ura){uraO.show();}
 
 //Neptune
-    nepO = new Orbit(data.nep.elements[0].a, data.nep.elements[0].e);
+    var nepO = new Orbit(data.nep.elements[0].a, data.nep.elements[0].e);
     if(nep){nepO.show();}
 }
--- p/solar-system/scripts/sketch.js
+++ p/solar-system/scripts/sketch.js
@@ -1,55 +1,94 @@
-var scl;
-var startingscl;
+var scl = 1;
+var newscl = 1;
+var canvas;
 var solar_r;
+var myFont;
+var nepA;
+var nepB;
+var nepE;
 
-function fromAUtoPx(au){
+function fromAUtoPx(au) {
+    //A function to convert between astronomical units and pixels, based on: solar radius = 1/215th of an AU
     return au * solar_r * 215;
 }
+
 
 function setup() {
 
     //Canvas and stars
-        canvas = createCanvas(document.getElementById('canvas').clientWidth, document.getElementById('canvas').clientHeight);
-        canvas.parent('canvas');
-        bg = new StarBG(stardensity);
-        bg.show();
+    canvas = createCanvas(document.getElementById('canvas').clientWidth, document.getElementById('canvas').clientHeight);
+    canvas.parent('canvas');
 
-    //Sun
-        scl = 1/ (document.getElementById('canvas').clientWidth  * 5 );
-        startingscl = scl;
+    var hammertime = new Hammer(canvas.elt);
+    hammertime.get('pinch').set({ enable: true });
+    hammertime.on('pinch', function () {
+        document.getElementById('heading').innerHTML = "I PINCHED";
+    });
+
+    bg = new StarBG(stardensity);
+    bg.show();
+
+    myFont = loadFont('/p/solar-system/fonts/Roboto-Light.ttf');
+
+    //Sun radius determines entire scale: most zoomed out is where orbit of neptune will fit on the canvas
+    nepA = data.nep.elements[0].a;
+    nepE = data.nep.elements[0].e;
+    nepB = nepA * Math.sqrt(1 - (nepE * nepE));
 
 }
 
 function draw() {
+    //Scale
+    // Set sun radius but give it 20px of space on either side extra. This is the beginning scale (scl = 1)
+    solar_r = Math.min((height-20) / (2 * nepB * 215), (width-20) / (2 * nepA * 215));
+
+    //Scaling: every frame, lerp between current scl and the one we should get.
+    scl = lerp(scl, newscl, 0.15);
+
+    //scale it up
+    solar_r *= scl;
+
+    //Drawing
+    //Stars
     bg.show();
 
-    solar_r = document.getElementById('canvas').clientWidth * scl ;
+    //Sun
     fill('#fff941');
-    textFont('Segoe UI');
-    text("Sun", width/2, (height/2)-(solar_r*2));
+    ellipse(width / 2, height / 2, solar_r*2, solar_r*2);
+
+    textFont(myFont);
+    textSize(12);
+    text("Sun", width / 2 + 1.5*solar_r, height / 2);
+
 
     //Orbits
-    ellipse(width/2, height /2, solar_r, solar_r);
-    showOrbits(true,true,true,true,true,true,true,true);
+    showOrbits(true, true, true, true, true, true, true, true);
 
 }
 
 function mouseWheel(event) {
-    // if we are zoomed in, zooming in and out is permitted
-    if(scl > startingscl) {
-        scl += 1 / (100 * (-event.delta));
-    //if we reached the edge, only zooming in is permitted
-    } else if( scl + 1 / (100 * (-event.delta)) >= startingscl) {
-        scl += 1 / (100 * (-event.delta));
-    // if we try to zoom out past that, scl stays on value
+
+    // get the event delta
+    var d = -event.delta;
+    // for a normal scroll of +-100, we want a factor  +-1.5 for zoom, so
+    d = (d / 200) * 3;
+    // we want the newscl to be a product when zooming in, and a division when zooming out
+    newscl = scl * Math.pow(Math.abs(d), Math.sign(d));
+
+    //don't go below the 100% level pls
+    if (newscl < 1) {
+        newscl = 1;
+    }
+    //don't go further than sun-in-your-face pls
+    if (solar_r > Math.sqrt(width*width + height * height)/2 && d>0){
+        newscl = scl;
     }
 }
 
-window.onresize = function() {
+window.onresize = function () {
     noLoop();
     var sclbeforeresize = scl;
     setup();
     scl = sclbeforeresize;
     loop();
 };
-
